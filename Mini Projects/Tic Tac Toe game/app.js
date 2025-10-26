8d7dee7fe98a53457e42bc802f3e558c226405f9
Kishan_vaghela <designevelop@gmail.com>
2025-10-26 08:42:22 +0530

    Tsak Compalet

--- Mini Projects/Tic Tac Toe game/app.js
+++ Mini Projects/Tic Tac Toe game/app.js
@@ -17,61 +17,67 @@ const winPatterns = [
     [6, 7, 8]
 ]
 
-const resetGame = () =>{
-  turn0 = true;
-  enableBoxes();
-  msgContainer.classList.add("hide");
-    
-}
-
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
         console.log("clicked");
         if (turn0) { //player0
-            box.innerText = "0";
+            box.innerText = "0"; 
             turn0 = false;
-             box.style.color = "#FF3F7F";
+            box.style.color = "#FF3F7F";
         } else {//playerX
             box.innerText = "X";
             turn0 = true;
-             box.style.color = "#8C00FF";
+            box.style.color = "#8C00FF";
         }
         box.disabled = true;
+        checkWinner()
 
-                                                
     });
 })
 
-const disabledBoxes = () =>{
-    for(let box of boxes){
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+
+        if (pos1val != "" && pos2val != "" && pos3val != "") {
+            if (pos1val === pos2val && pos2val === pos3val) {
+                showWinner(pos1val)
+                return;
+            }
+        }
+    }
+    // Optional: check for draw
+
+}
+
+const resetGame = () => {
+    turn0 = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+
+}
+
+const showWinner = (winner) => {
+    msgs.innerText = `Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disabledBoxes()
+}
+
+const disabledBoxes = () => {
+    for (let box of boxes) {
         box.disabled = true;
     }
 }
-const enableBoxes = () =>{
-    for(let box of boxes){
+const enableBoxes = () => {
+    for (let box of boxes) {
         box.disabled = false;
         box.innerText = "";
     }
 }
 
-const showWinner = (winner) =>{ 
-    msgs.innerText = `Winner is ${winner}`;
-    msgContainer.classList.remove("hide");
-    disabledBoxes ()
-}
 
-const checkWinner = () => {
-    for (let pattern of winPatterns) {
-        let pos1val = boxes[pattern[0]].innerHTML;
-        let pos2val = boxes[pattern[1]].innerHTML;
-        let pos3val = boxes[pattern[2]].innerHTML;
 
-        if (pos1val != "" && pos2val != "" && pos3val != "") {
-            if (pos1val === pos2val && pos2val === pos3val) {
-                showWinner(pos1val)
-            }
-        }
-    }
-}
-newGameBtn.addEventListener("click", resetGame );
-resetBtn.addEventListener("click", resetGame );
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
